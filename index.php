147b1f3e6d0eabb1874fb6807742ec58dcfe3781
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-11-28 21:28:31 +0100

    access to the methodes get and set

--- index.php
+++ index.php
@@ -1,46 +1,52 @@
 <?php
 
-// creat a class
-class apprenant 
-{
-    //les proprieties
-    const pi = 3.14;
-    public $name = "mouad";
-    public $age = 24;
-    public $code = 55;
-
-    //Les methodes 
-    // getters and setters 
-    public function getName()
+    // creat a class
+    class apprenant 
     {
-          return $this->name;
+        //les proprieties
+        const pi = 3.14;
+        public $name = "mouad";
+        public $age = 24;
+        public $code = 55;
+
+        //Les methodes 
+        // getters and setters 
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        public function setName($tempName) 
+        {
+            $this->name = $tempName;
+        }
+
     }
 
-    public function setName($tempName) 
-    {
-        $this->name = $tempName;
-    }
-
-}
-
-//creat an object 
-$myobject = new apprenant();
+    //creat an object 
+    $myobject = new apprenant();
 
 
-//access to the prorieties
-$myobject->name .= "fifel";
-echo $myobject->name."\n";
+    //access to the prorieties
+    $myobject->name .= "fifel";
+    echo $myobject->name."\n";
 
 
-$myobject->age += 6;
-echo $myobject->age."\n";
+    $myobject->age += 6;
+    echo $myobject->age."\n";
 
 
-//access to the const
-    //1 - using the object (::scope resolution operator) 
-    echo $myobject::pi."\n"; 
+    //access to the const
+        //1 - using the object (::scope resolution operator) 
+        echo $myobject::pi."\n"; 
 
-    //2 - using the class (::scope resolution operator) 
-    echo apprenant::pi; 
+        //2 - using the class (::scope resolution operator) 
+        echo apprenant::pi."\n"; 
+
+    //accesss to the methodes 
+        $myobject->setName("aymen");
+        echo $myobject->name."\n";  
+        // or    
+        echo $myobject->getName()."\n";
 
 ?>
