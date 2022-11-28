46e57fba37eaec0c84215e8da785796784123e80
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-11-28 20:52:03 +0100

    getters and sitters methodes (functions)

--- index.php
+++ index.php
@@ -9,17 +9,32 @@ class apprenant
     public $age = 24;
     public $code = 55;
 
+    //Les methodes 
+    // getters and setters 
+    public function getName()
+    {
+          return $this->name;
+    }
+
+    public function setName($tempName) 
+    {
+        $this->name = $tempName;
+    }
+
 }
 
 //creat an object 
 $myobject = new apprenant();
 
+
 //access to the prorieties
 $myobject->name .= "fifel";
 echo $myobject->name."\n";
 
+
 $myobject->age += 6;
 echo $myobject->age."\n";
+
 
 //access to the const
     //1 - using the object (::scope resolution operator) 
@@ -27,7 +42,5 @@ echo $myobject->age."\n";
 
     //2 - using the class (::scope resolution operator) 
     echo apprenant::pi; 
-
-
 
 ?>
